6ce6e0e17a63a94dc1e4be2d26241318dae90207
Thiordev221 <thiorabdoulaye39@gmail.com>
2026-01-05 16:45:36 +0000

    modifications sur application.properties

--- Gestion_des_Rendezvous/src/main/java/com/thiordev/Gestion_des_Rendezvous/config/CorsConfig.java
+++ Gestion_des_Rendezvous/src/main/java/com/thiordev/Gestion_des_Rendezvous/config/CorsConfig.java
@@ -1,3 +1,5 @@
+package com.thiordev.Gestion_des_Rendezvous.config;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
